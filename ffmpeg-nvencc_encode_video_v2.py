4401fcd0f61e2ca9e16464c38c1bde05ac7f9609
seiya-git <seiya.loveless.mail@gmail.com>
2022-11-27 16:31:27 +0300

    update

--- ffmpeg-nvencc_encode_video_v2.py
+++ ffmpeg-nvencc_encode_video_v2.py
@@ -8,36 +8,54 @@ import re
 import time
 import subprocess
 
+import argparse
+import json
 from pathlib import Path
 from pathlib import PurePath
 
+import questionary
+from questionary import Choice, Validator, ValidationError, prompt
+
+extFile = ['.mkv', '.mp4', '.mov', '.avs']
 reName = re.compile(r'^(?P<title>.+?( \d|\(\d+\))?)( -)? (?P<episode>(\d+|SP( )?(\d+)|OVA( )?(\d+)?)(v\d)?)$')
 reClean = re.compile(r'^(\[[^\[\]]*?\]|\([^\(\)]*?\)) | (\[[^\[\]]*?\]|\([^\(\)]*?\))$')
 
-import questionary
-from questionary import Choice
+# int validator
+def IntValidator(text):
+    if len(text) == 0:
+        return 'Please enter a value'
+    try:
+        int(text)
+    except ValueError:
+        return 'Please enter a value'
+    return True
 
-def checkYN(inputStr: str):
-    inputStr = str(inputStr).lower().strip()
-    if len(inputStr) > 0 and inputStr[0] == 'y':
-        return True
-    else:
-        return False
-
-def askYN(question: str):
-    ask = input(f'{question}(y/N) ')
-    return checkYN(ask)
-
-def getVideoSize(inputVideoFile: Path):
-    # ffprobe -v error -hide_banner -print_format json -show_streams -select_streams v
+# get data from video file
+def getVideoData(inputVideo: Path, streamType: str):
     result = subprocess.run([
         r'ffprobe', '-v', 'error', '-hide_banner',
-        '-select_streams', 'v:0', '-show_entries', 'stream=width,height',
-        '-of', 'default=noprint_wrappers=1:nokey=1', inputVideoFile
+        '-print_format', 'json', '-show_format', '-show_streams', 
+        '-select_streams', streamType,
+        inputVideo
     ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    result = result.stdout.decode('utf-8').replace(r'\r\n', r'\n')
-    return list(map(int, result.splitlines()[-2:]))
+    result = json.loads(result.stdout.decode('utf-8'))
+    return result
 
+# find subs files
+def searchSubsFile(inputVideo: Path):
+    subsData = argparse.Namespace()
+    subsData.root = PurePath(inputVideo).parent
+    subsData.prefix = PurePath(inputVideo).stem
+    subsData.files = []
+    if PurePath(inputVideo).suffix.lower() == '.mkv':
+        subsData.files.extend([os.path.join(os.path.sep, subsData.root, PurePath(inputVideo).name)])
+    for root, dirs, files in os.walk(subsData.root):
+        for file in files:
+            if file.startswith(f'{subsData.prefix}') and file.lower().endswith('.ass'):
+                subsData.files.append(os.path.join(os.path.sep, root, file))
+    return subsData
+
+# fix path for ffmpeg
 def fixPath(inFile: Path, forFFmpeg: bool):
     inFile = inFile.replace('\\', '/')
     if forFFmpeg:
@@ -48,40 +66,29 @@ def fixPath(inFile: Path, forFFmpeg: bool):
             inFile = inFile.replace("'", r"'\''")
     return inFile
 
-## process file
 def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio: bool, subsTrackIndex: int, subsFileIndex: int):
-    inFile  = os.path.abspath(inFile)
+    # inFile  = os.path.abspath(inFile)
     inDir   = PurePath(inFile).parent
     inFonts = fixPath(f'{inDir}/fonts', useFFmpeg)
-    inSubs  = []
+    inSubs  = searchSubsFile(inFile).files
     
-    if PurePath(inFile).suffix.lower() == '.mkv':
-        inSubs.append(inFile)
+    videoData = getVideoData(inFile, 'v:0')['streams']
+    if len(videoData) < 0:
+        print(f':: Skipping: {PurePath(inFile).name}')
+        return
     
-    for root, dirs, files in os.walk(inDir):
-        for file in files:
-            if file.startswith(f'{PurePath(inFile).stem}') and file.lower().endswith('.ass'):
-                inSubs.append(os.path.join(os.path.sep, root, file))
-    
-    srcVideoSize = getVideoSize(inFile)
-    outVideoSize = [[ str(srcVideoSize[0]), 'orig' ]]
+    videoData = videoData[0]
+    outVideoSize = [[ str(videoData['width']), 'orig' ]]
     audioBitrate = '192'
     
-    if os.environ.get('VEncResizeExt') == '1':
-        if srcVideoSize[0] >= 7680 or srcVideoSize[1] >= 4320:
-            outVideoSize.append(['3840', '2160'])
-        if srcVideoSize[0] >= 3840 or srcVideoSize[1] >= 2160:
-            outVideoSize.append(['2560', '1440'])
-    
-    if not os.environ.get('skipVEncResize') == '1':
-        if srcVideoSize[0] >= 2560 or srcVideoSize[1] >= 1440:
-            outVideoSize.append(['1920', '1080'])
-        if srcVideoSize[0] >= 1920 or srcVideoSize[1] >= 1080:
-            outVideoSize.append(['1280', '720'])
+    if videoData['width'] >= 2560 or videoData['height'] >= 1440:
+        outVideoSize.append(['1920', '1080'])
+    if videoData['width'] >= 1920 or videoData['height'] >= 1080:
+        outVideoSize.append(['1280', '720'])
     
     isGifv = False
     if os.environ.get('toGifv') != None and os.environ.get('toGifv') == '1':
-        print(':: Convert to GIFV!')
+        print('\n:: Convert to GIFV!')
         outVideoSize = [[]]
         outVideoSize.append([ '512', '512' ])
         outVideoSize.append([ '848', '480' ])
@@ -124,7 +131,7 @@ def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio:
             encCmd.extend([ r'ffmpeg', '-hide_banner' ])
             encCmd.extend([ '-i', inFile ]);
             encCmd.extend([ '-map_metadata', '-1', '-map_chapters', '-1' ])
-            encCmd.extend([ '-map', '0:v:0', '-c:v', 'libx264', '-crf', '20' ])
+            encCmd.extend([ '-c:v', 'libx264', '-crf', '20' ])
             encCmd.extend([ '-preset:v', 'faster', '-tune:v', 'animation' ])
         else:
             encCmd.extend([ r'NVEncC64' ])
@@ -137,7 +144,7 @@ def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio:
         vFilters = [ 'format=yuv420p' ]
         inSubsLog = ''
         
-        if len(inSubs) > 0 and subsTrackIndex > -1 and subsFileIndex > -1:
+        if len(inSubs) > 0 and subsFileIndex > -1 and subsTrackIndex > -1:
             if subsFileIndex > len(inSubs):
                 subsFileIndex = len(inSubs) - 1
             inSubsFile = fixPath(inSubs[subsFileIndex], useFFmpeg)
@@ -148,6 +155,8 @@ def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio:
                     inpSubsStr = inpSubsStr + f':stream_index={subsTrackIndex}'
                     inSubsLog = inSubsLog + f':track={subsTrackIndex}'
                 vFilters.append(f'subtitles={inpSubsStr}:fontsdir=\'{inFonts}\'')
+                # vFilters.append(f'[out][0:s:{subsTrackIndex}]overlay[out]')
+                # [video][scaledCoverPhoto]overlay=:x=w/2:y=(H-h)/2:enable='between(t,4,21)',subtitles=intro.ass:fontsdir=../../preview/fonts,format=yuv420p,scale=-2:494
             else:
                 inpSubsStr = f'filename="{inSubsFile}"'
                 if inSubsFile.lower().endswith('.mkv'):
@@ -156,7 +165,7 @@ def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio:
                 encCmd.extend([ '--vpp-subburn', f'{inpSubsStr},fontsdir="{inFonts}"' ])
         
         if x > 0:
-            byWidth = True if int(curVideoSize[0]) / (srcVideoSize[0]/srcVideoSize[1]) <= int(curVideoSize[1]) else False
+            byWidth = True if int(curVideoSize[0]) / (videoData['width']/videoData['height']) <= int(curVideoSize[1]) else False
             if useFFmpeg:
                 if byWidth:
                     vFilters.append(f'scale={curVideoSize[0]}:-2')
@@ -169,7 +178,8 @@ def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio:
                     encCmd.extend([ '--output-res', f'-2x{curVideoSize[1]}' ])
         
         if useFFmpeg:
-            encCmd.extend([ '-vf', ','.join(vFilters) ])
+            vFilters = ','.join(vFilters)
+            encCmd.extend([ '-filter_complex', f'[0:v:0]{vFilters}[outv]', '-map', '[outv]' ])
         
         if audioTrackIndex > -1:
             if useFFmpeg:
@@ -202,7 +212,7 @@ def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio:
         print(f'\n:: Encoding : {PurePath(inFile).name}')
         if inSubsLog != '':
             print(f':: Subtitles: {inSubsLog}')
-        print(  f':: Output   : {PurePath(outFile).name}\n')
+        print(f':: Output   : {PurePath(outFile).name}\n')
         
         # print(vFilters)
         # print('\n  '.join(encCmd) + '\n')
@@ -215,87 +225,94 @@ def encodeFile(inFile: Path, useFFmpeg: bool, audioTrackIndex: int, encodeAudio:
 
 # encode
 def configEncode(inputPath: Path):
-    print(f':: Selected path: {inputPath}\n')
-    fileExt = PurePath(inputPath).suffix.lower()
+    # filesArr
+    inpFile = []
+    
+    # check if dir
+    if os.path.isdir(inputPath):
+        for file in os.listdir(inputPath):
+            file = os.path.join(inputPath, file)
+            fileExt = PurePath(file).suffix.lower()
+            if extFile.count(fileExt) > 0:
+                inpFile.extend([file])
+    
+    # file to array
+    if os.path.isfile(inputPath):
+        inpFile.extend([inputPath])
+    
+    # check files
+    if len(inpFile) < 1:
+        print(':: No videos in folder!')
+        return
     
     # useNVEnc
-    if os.environ.get('useNVEnc') != None:
-        useNVEnc = checkYN(os.environ.get('useNVEnc'))
-    else:
-        useNVEnc = askYN(':: Use NVEnc? ')
+    useNVEnc = questionary.confirm('Use NVEnc (default=No):', default=False).ask()
     
     # useFFmpeg
     useFFmpeg = (not useNVEnc)
     
-    # audioTrackIndex
-    if os.environ.get('audioTrackIndex') != None:
-        audioTrackIndex = os.environ.get('audioTrackIndex')
-    else:
-        audioTrackIndex = input(':: Audio track index (-1: no audio): ')
+    # get audio data from first video
+    audioData = getVideoData(inpFile[0], 'a')['streams']
     
-    try:
-        audioTrackIndex = int(audioTrackIndex)
-    except ValueError:
-        audioTrackIndex = 0
+    # show tracks
+    print(f'\n:: Audio from first file:')
+    if len(audioData) > 0:
+        for t in range(len(audioData)):
+            a = audioData[t]
+            lang = a['tags']['language']
+            title = a['tags']['title'] if 'title' in a['tags'] else ''
+            print(f'[{t}] {lang} {title}')
+    else:
+        print(f'[-] No audio')
+    
+    # index
+    audioTrackIndex = int(questionary.text('Audio track index (no audio: -1):', default='0', validate=IntValidator).ask())
     
     if audioTrackIndex < -1:
         audioTrackIndex = -1
-    
     if audioTrackIndex > 1000:
         audioTrackIndex = 1000
     
-    # encodeAudio
+    encodeAudio = False
     if audioTrackIndex > -1:
-        if os.environ.get('encodeAudio') != None:
-            encodeAudio = checkYN(os.environ.get('encodeAudio'))
-        else:
-            encodeAudio = askYN(':: Encode audio? ')
-    else:
-        encodeAudio = False
+        print('')
+        encodeAudio = questionary.confirm('Encode audio (default=No):', default=False).ask()
+        print('')
     
-    # subsTrackIndex
-    if fileExt != '.mp4' and fileExt != '.mov' and fileExt != '.avs':
-        if os.environ.get('subsTrackIndex') != None:
-            subsTrackIndex = os.environ.get('subsTrackIndex')
-        else:
-            subsTrackIndex = input(':: Subtitle track index for hardsubs (-1: Skip): ')
-    else:
-        subsTrackIndex = 0
+    subsData = searchSubsFile(inpFile[0])
+    if len(subsData.files) > 0:
+        print(f':: Subtitles for first file:')
+        for t in range(len(subsData.files)):
+            print(f'[{t}] {PurePath(subsData.files[t]).name}')
     
-    try:
-        subsTrackIndex = int(subsTrackIndex)
-    except ValueError:
-        subsTrackIndex = 0
-    
-    if subsTrackIndex < -1:
-        subsTrackIndex = -1
-    
-    # subsFileIndex
-    if os.environ.get('subsFileIndex') != None:
-        subsFileIndex = os.environ.get('subsFileIndex')
-    else:
-        subsFileIndex = input(':: Subtitle file index for hardsubs (-1: Skip): ')
-    
-    try:
-        subsFileIndex = int(subsFileIndex)
-    except ValueError:
-        if fileExt == '.mkv':
-            subsFileIndex = 0
-        else:
-            subsFileIndex = -1
+    subsFileDefault = '0' if PurePath(inpFile[0]).suffix.lower() == '.mkv' else '-1'
+    subsFileIndex = int(questionary.text('Subtitle file index for hardsubs (-1: Skip):', default=subsFileDefault, validate=IntValidator).ask())
     
     if subsFileIndex < -1:
         subsFileIndex = -1
+    if subsFileIndex > 1000:
+        subsFileIndex = 1000
     
-    if os.path.isfile(inputPath):
-        encodeFile(inputPath, useFFmpeg, audioTrackIndex, encodeAudio, subsTrackIndex, subsFileIndex)
+    subsTrackIndex = 0
+    if PurePath(inpFile[0]).suffix.lower() == '.mkv' and subsFileIndex == 0:
+        subsData = getVideoData(inpFile[0], 's')['streams']
+        print(f'\n:: Subtitles from first file:')
+        if len(subsData) > 0:
+            for t in range(len(subsData)):
+                s = subsData[t]
+                lang = s['tags']['language']
+                title = s['tags']['title'] if 'title' in s['tags'] else ''
+                print(f'[{t}] {lang} {title}')
+        else:
+            print(f'[-] No subtitles')
+        subsTrackIndex = int(questionary.text('Subtitle track index for hardsubs (-1: Skip):', default='0', validate=IntValidator).ask())
+        if subsTrackIndex < -1:
+            subsTrackIndex = -1
+        if subsTrackIndex > 1000:
+            subsTrackIndex = 1000
     
-    if os.path.isdir(inputPath):
-        for file in os.listdir(inputPath):
-            file = os.path.join(inputPath, file)
-            fileLow = file.lower()
-            if fileLow.endswith('.mkv') or fileLow.endswith('.mp4') or fileLow.endswith('.avs'):
-                encodeFile(file, useFFmpeg, audioTrackIndex, encodeAudio, subsTrackIndex, subsFileIndex)
+    for i in range(len(inpFile)):
+        encodeFile(inpFile[i], useFFmpeg, audioTrackIndex, encodeAudio, subsTrackIndex, subsFileIndex)
 
 # set folder
 if len(sys.argv) < 2:
@@ -309,14 +326,18 @@ inputPath = os.path.abspath(inputPath)
 # check path
 if os.path.isfile(inputPath):
     fileExt = PurePath(inputPath).suffix.lower()
-    if fileExt == '.mkv' or fileExt == '.mp4' or fileExt == '.mov' or fileExt == '.avs' or fileExt == '.gif':
+    if extFile.count(fileExt) > 0:
+        print(f':: Input file: {inputPath}')
         configEncode(inputPath)
     else:
-         print(f':: Path is not a video file: "{inputPath}"!')
+        print(f':: Input file is not a video file: {inputPath}')
+        
 elif os.path.isdir(inputPath):
+    print(f':: Input folder: {inputPath}')
     configEncode(inputPath)
 else:
-    print(f':: Path is not a folder or video file: "{inputPath}"!')
+    print(f':: Input path is not a folder or video file: {inputPath}')
+
 
 # end
 if os.environ.get('isBatch') is None:
